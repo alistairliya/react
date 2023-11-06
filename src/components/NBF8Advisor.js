f512d33bb7e6cc14514c92e7c9e9e871a9ffbe27
Eugene Lin <eugenelin89@gmail.com>
2023-11-06 15:20:32 -0800

    showing advisor username. still quite a few bugs

--- src/components/NBF8Advisor.js
+++ src/components/NBF8Advisor.js
@@ -110,12 +110,7 @@ const NBF8Advisor = ({
             <p>{id}</p>
             <p>{advisor? JSON.stringify(advisor):""}</p>
             <p>{
-                        selectedAdvisors[id] && 
-                        selectedAdvisors[id].advisor && 
-                        selectedAdvisors[id].advisor.first_name && 
-                        selectedAdvisors[id].advisor.last_name? 
-                            selectedAdvisors[id].advisor.first_name+' '+selectedAdvisors[id].advisor.last_name:
-                            'Advisor'
+                advisor? advisor.username:""
             }</p>
             <div className="form-control">
                 <label>Advisor:</label>
@@ -124,13 +119,16 @@ const NBF8Advisor = ({
                     isDisabled={!writeAccess}
                     options={advisorOptions}
                     placeholder={
-                        selectedAdvisors[id] && 
-                        selectedAdvisors[id].advisor && 
-                        selectedAdvisors[id].advisor.first_name && 
-                        selectedAdvisors[id].advisor.last_name? 
-                            selectedAdvisors[id].advisor.first_name+' '+selectedAdvisors[id].advisor.last_name:
-                            'Advisor'
-                        }//'Select Advisor'
+                        //selectedAdvisors[id] && 
+                        //selectedAdvisors[id].advisor && 
+                        //selectedAdvisors[id].advisor.first_name && 
+                        //selectedAdvisors[id].advisor.last_name? 
+                        //    selectedAdvisors[id].advisor.first_name+' '+selectedAdvisors[id].advisor.last_name:
+                        //    'Advisor'
+                        advisor? (advisor.first_name.trim()!==''&&advisor.last_name.trim()!==''?advisor.first_name+' '+advisor.last_name: (advisor.username?advisor.username:"?")):"Select Advisor"
+                        
+                    }//'Select Advisor'
+                        
                     onChange={(selectedOption)=>{
                         console.log('selectedOption')
                         console.log(selectedOption)
