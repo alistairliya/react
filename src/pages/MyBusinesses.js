f9b504982d7893eb092e3e2458adc4b4e80e9fdd
Eugene Lin <eugenelin89@gmail.com>
2023-12-19 15:44:42 -0800

    prep for pagination

--- src/pages/MyBusinesses.js
+++ src/pages/MyBusinesses.js
@@ -1,23 +1,20 @@
-
-
-import {useState, useEffect} from 'react'
-import Booga from '../components/Header'
-import Footer from '../components/Footer'
-import Businesses from '../components/Businesses'
-import NewBusiness from '../components/NewBusiness' 
-import BusinessDetails from '../components/BusinessDetails'
-//import About from '../components/About' 
+import { useState, useEffect } from "react";
+import Booga from "../components/Header";
+import Footer from "../components/Footer";
+import Businesses from "../components/Businesses";
+import NewBusiness from "../components/NewBusiness";
+import BusinessDetails from "../components/BusinessDetails";
+//import About from '../components/About'
 import { useAuth } from "../hooks/useAuth";
-import {ROOT_URL, FE_URL} from '../constants'
+import { ROOT_URL, FE_URL } from "../constants";
 function MyBusinesses() {
+  const { user } = useAuth();
+  const [showAddBusiness, setShowAddBusiness] = useState(false);
+  const [detailedBusiness, setDetailedBusiness] = useState(null); // for BusinessDetails
+  const [refreshTrigger, setRefreshTrigger] = useState(0);
 
-    const { user } = useAuth();
-  const [showAddBusiness, setShowAddBusiness] = useState(false)
-  const [detailedBusiness, setDetailedBusiness] = useState(null) // for BusinessDetails
-  const [refreshTrigger , setRefreshTrigger] = useState(0)
-
-  const [businesses, setBusinesses] = useState(
-    [/*
+  const [businesses, setBusinesses] = useState([
+    /*
       {
         "id": 1,
         "business_type": ROOT_URL+"/api/businesstype/1/",
@@ -46,92 +43,108 @@ function MyBusinesses() {
         "modified_date": "2022-12-12T15:52:00Z",
         "highlighted": false
       }*/
-    ]
-  )
+  ]);
 
-  useEffect(()=>{
-    const getBusinesses = async() => {
-      const businessesFromServer = await fetchBusiness()
-      //setBusinesses(businessesFromServer.reverse())      
-      setBusinesses(businessesFromServer)      
-    }
-    getBusinesses()
-    console.log("user:")
-    console.log(user)
-  }, [showAddBusiness, refreshTrigger])
+  // Pagication startss here
+  useEffect(() => {
+    const getBusinesses = async () => {
+      const businessesFromServer = await fetchBusiness();
+      //setBusinesses(businessesFromServer.reverse())
+      setBusinesses(businessesFromServer);
+    };
+    getBusinesses();
+    console.log("user:");
+    console.log(user);
+  }, [showAddBusiness, refreshTrigger]);
 
-  const fetchBusiness = async()=>{
-    let headers = new Headers()
-    const token = user['token']
-    console.log('About to fetch with token '+token)
-    //const auth_str = 'Basic '+btoa('test:test123!') 
-    
-    const auth_str = 'Token '+token 
-    console.log(auth_str)
-    headers.set('Authorization', auth_str)
-    const res = await fetch(ROOT_URL+'/api/mybusiness/',{headers:headers})
-    const data = await res.json()
-    console.log(data)
-    return data
-  }
+  const fetchBusiness = async () => {
+    let headers = new Headers();
+    const token = user["token"];
+    console.log("About to fetch with token " + token);
+    //const auth_str = 'Basic '+btoa('test:test123!')
+
+    const auth_str = "Token " + token;
+    console.log(auth_str);
+    headers.set("Authorization", auth_str);
+    const res = await fetch(ROOT_URL + "/api/mybusiness/", {
+      headers: headers,
+    });
+    const data = await res.json();
+    console.log(data);
+    return data;
+  };
 
   // from Add Task example, 1:09:33
   const addBusiness = (business) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    console.log(id)
-    console.log(business)
-    const newBusiness = {id, ...business}
-    setBusinesses([...businesses, newBusiness])
-  }
-  
+    const id = Math.floor(Math.random() * 10000) + 1;
+    console.log(id);
+    console.log(business);
+    const newBusiness = { id, ...business };
+    setBusinesses([...businesses, newBusiness]);
+  };
+
   // from Delete Task in example, 52:31, deleteTask
-  const editBusiness = (business) =>{
-    console.log('edit '+business.id)
-    console.log(business)
-    setDetailedBusiness(business)
+  const editBusiness = (business) => {
+    console.log("edit " + business.id);
+    console.log(business);
+    setDetailedBusiness(business);
     //setBusinesses(businesses.filter((business)=>business.id!=id)) // example from deleting task 55:30
-  }
+  };
 
   const closeBusinessDetailsComponent = () => {
-    setDetailedBusiness(null)
-  }
+    setDetailedBusiness(null);
+  };
 
   // Using toggleReminder in example 57:53
   const toggleReminder = (id) => {
-    console.log('toggle', id)
-    setBusinesses(businesses.map((business)=>business.id === id? {...business, highlighted: !business.highlighted }: business))
-  }
+    console.log("toggle", id);
+    setBusinesses(
+      businesses.map((business) =>
+        business.id === id
+          ? { ...business, highlighted: !business.highlighted }
+          : business
+      )
+    );
+  };
 
   const closeAddBusinessComponent = () => {
-    setShowAddBusiness(false)
-  }
+    setShowAddBusiness(false);
+  };
 
   // Triggers a refresh of this component.
   const refreshBusinesses = () => {
-    console.log('refreshing...')
-    setRefreshTrigger(refreshTrigger+1)
-  }
+    console.log("refreshing...");
+    setRefreshTrigger(refreshTrigger + 1);
+  };
 
   return (
     <div className="container">
-       <div>
-        <a href={FE_URL+"/me/dashboard"}>Dashboard</a>
-        </div>
-      <Booga 
-        title='My Businesses'
-        onAdd={()=>setShowAddBusiness(!showAddBusiness)}
-        showAdd = {showAddBusiness}
+      <div>
+        <a href={FE_URL + "/me/dashboard"}>Dashboard</a>
+      </div>
+      <Booga
+        title="My Businesses"
+        onAdd={() => setShowAddBusiness(!showAddBusiness)}
+        showAdd={showAddBusiness}
       />
-      {showAddBusiness && <NewBusiness onAdd={addBusiness} close={closeAddBusinessComponent} />}
-      {detailedBusiness && <BusinessDetails business={detailedBusiness} closeComponent={closeBusinessDetailsComponent} refreshBusinesses={refreshBusinesses} />}
-      {businesses.length > 0?(
-        <Businesses 
-          businesses = {businesses} 
-          onEdit = {editBusiness} 
+      {showAddBusiness && (
+        <NewBusiness onAdd={addBusiness} close={closeAddBusinessComponent} />
+      )}
+      {detailedBusiness && (
+        <BusinessDetails
+          business={detailedBusiness}
+          closeComponent={closeBusinessDetailsComponent}
+          refreshBusinesses={refreshBusinesses}
+        />
+      )}
+      {businesses.length > 0 ? (
+        <Businesses
+          businesses={businesses}
+          onEdit={editBusiness}
           onToggle={toggleReminder}
-        /> 
-      ):( 
-        'No business to show'
+        />
+      ) : (
+        "No business to show"
       )}
       <Footer />
     </div>
