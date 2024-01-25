4d4ce1c77f8c7ff07df86b67d38ca80720448d94
Eugene Lin <eugenelin89@gmail.com>
2024-01-25 13:32:13 -0800

    cleanup, removing console.lg

--- src/components/Business.js
+++ src/components/Business.js
@@ -24,7 +24,6 @@ const Business = ({business, onEdit, onToggle}) => {
   const [status, setStatus] = useState({})
 
   let advisor = business['created_by']
-  //console.log('----------------------') 
   if(business.related_users.length >0){ 
     for(let related_user of business.related_users){
       if(related_user['user_role'] && related_user['user_role']['user_role_name']==='owner'){
@@ -32,8 +31,6 @@ const Business = ({business, onEdit, onToggle}) => {
       }
     }
   }
-  //console.log('advisor...:')
-  //console.log(advisor)
   
   if(advisor){
     var advisor_name = advisor['username']
@@ -41,7 +38,6 @@ const Business = ({business, onEdit, onToggle}) => {
     if(advisor['first_name'] && advisor['last_name']&&(advisor['first_name'].trim()!==''||advisor['last_name'].trim()!=='')){
       advisor_name = advisor['first_name']+' '+advisor['last_name']
     }
-    //console.log('advisor name: '+advisor_name)
   }
 
   const fetchObject = async (url) =>{
@@ -56,17 +52,13 @@ const Business = ({business, onEdit, onToggle}) => {
 
   // This is inefficient. A better way is to get all the statuses in the parent component and pass down a dictionary so we only need to do this once.
   const getStatus = async () => {
-    console.log('inside getStatus in Business.js')
     let s = await fetchObject(business.status)
-    console.log(s)
     setStatus(s)
     return s.status_name
   }
 
   const {user} = useAuth()
   useEffect(()=>{
-    console.log('useEffect for Business ID: '+business.id)
-    console.log(business.status)
     const url = business.client
     const fetchResource = async () =>{
         let headers = new Headers()
@@ -75,13 +67,10 @@ const Business = ({business, onEdit, onToggle}) => {
         headers.set('Authorization', auth_str)
         const res = await fetch(url,{headers:headers})
         const data = await res.json()
-        //console.log("\n***CLIENT: ")
-        //console.log(data)
         setClient(data)
         return data
     }
     if (client.id === undefined){
-        //console.log('fetching client')
         fetchResource() // fetching clint for each business 
     }
     getStatus() // fetching status for each businesss
@@ -90,13 +79,11 @@ const Business = ({business, onEdit, onToggle}) => {
 
 
   const foo =()=>{
-    console.log('inside foo')
     //getStatus()
     return business.status
   }
 
   const showPdfLink = () =>{
-    console.log(business)
     if(business.files.length>0){
       const url = business.files[business.files.length - 1].file
       const onClick = () =>{
